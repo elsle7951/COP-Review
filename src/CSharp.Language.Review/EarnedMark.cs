33bf05d9fc077402d1b744e007461b716df8ef9c
elsle7951 <46492882+elsle7951@users.noreply.github.com>
2019-01-08 09:36:48 -0700

    Typing code

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -8,5 +8,17 @@ namespace CSharp.Language.Review
 {
     class EarnedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned;  }
+            set
+            {
+                if (value < 0 || value > Possible)
+                            throw new Exception("Invalid earned mark assigned");
+                        _Earned = value;                
+            }
+        }
     }
 }
